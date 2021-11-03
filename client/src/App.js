23888a773712f45eeb999cb9e774924e20185217
clartor <clara.tornblom@gmail.com>
2021-11-03 13:21:06 +0100

    todo list comp

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import todo from './component/todo';
+import login from './component/login/Login';
+import register from './component/register';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+
+    <div className="App">
+      <header>
+        HEJ
+      </header>
+
+      <Switch>
+      <Route path="/todo" component={todo} />
+      </Switch>
+
+     
+    </div>
+    </Router>
+  );
+}
+
+export default App;
