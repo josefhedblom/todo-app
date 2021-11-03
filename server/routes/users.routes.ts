58843314f4e3d9ed2df46b238b51e0688d91cccd
Josef Hedblom <hedblomjosef@gmail.com>
2021-11-03 13:29:32 +0100

    Delete server

--- server/routes/users.routes.ts
+++
@@ -1,8 +0,0 @@
-import Express from 'express';
-const UserRouter = Express.Router()
-import { signup } from '../controllers/users.controllers';
-
-UserRouter.post('/signup', signup);
-
-export default UserRouter
-
